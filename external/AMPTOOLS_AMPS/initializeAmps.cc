5409f4d339f5f5ad97adfad5d36a963501fbaeb3
lan13005 <lng1492@gmail.com>
2023-09-25 10:58:09 -0400

    feat: adding amptools amps and dataio sources and makefiles

---
+++ external/AMPTOOLS_AMPS/initializeAmps.cc
@@ -0,0 +1,5 @@
+#include "initializeAmps.h"
+
+void initializeAmps(){
+    std::cout << "Initializing AMPTOOLS_AMPS for PyROOT..." << std::endl;
+}
---
+++ external/AMPTOOLS_DATAIO/initializeDataIO.cc
@@ -0,0 +1,5 @@
+#include "initializeDataIO.h"
+
+void initializeDataIO(){
+    std::cout << "Initializing AMPTOOLS_DATAIO for PyROOT..." << std::endl;
+}
