717e121326ef37e11adc0af4a859e76482de03cf
lan13005 <lng1492@gmail.com>
2023-09-26 09:31:24 -0400

    fix: add utils.py needed by fit program to access process ids

---
+++ utils/utils.py
@@ -0,0 +1,27 @@
+import os
+import psutil
+
+def get_function_by_pid(pid):
+    ''' Returns the function or executable associated with a process ID '''
+    try:
+        process = psutil.Process(pid)
+        function = process.name()  # This retrieves the name of the function or executable associated with the process.
+        return function
+    except psutil.NoSuchProcess:
+        return "Process with PID {} not found.".format(pid)
+    except psutil.AccessDenied:
+        return "Access denied to process with PID {}.".format(pid)
+    
+def get_pid_family():
+    ''' 
+    Returns the function or executable associated with the current process ID and its parent process ID
+
+    Example:
+        1) python example.py              -> python, bash
+        2) mpirun -np 1 python example.py -> python, mpirun
+    '''
+    pid = os.getpid()
+    ppid = os.getppid()
+    current = get_function_by_pid(pid)
+    parent = get_function_by_pid(ppid)
+    return current, parent
