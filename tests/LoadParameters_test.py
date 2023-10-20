3bdc297cc0a727aca0f74e3d90a01fac348c5f6c
lan13005 <lng1492@gmail.com>
2023-10-20 16:57:20 -0400

    test: update LoadParameters test

--- tests/LoadParameters_test.py
+++ tests/LoadParameters_test.py
@@ -18,6 +18,8 @@ def performCheck(input, truths):
     # cfgInfo.display()
 
     ############# TEST LOADING OF PARAMETERS #############
+    print(f'paramCtrl.params = {paramCtrl.params}')
+    print(f'true_params = {true_params}')
     assert( paramCtrl.params ==  true_params )
     assert( paramCtrl.paramsIsReal == true_paramsIsReal )
 
@@ -69,11 +71,11 @@ def test_result_LoadParameters():
     true_params = {'etapi::imZ::resAmp1': (462.405000586143+0j),
                    'etapi::imZ::resAmp2': (279.819030071884-16.633970687601j),
                    'etapi::imZ::resAmp3': (406.191515689351+164.391119358749j),
-                   'mass1': 1.0, 'mass2': 2.0, 'width2': 200.0, 'width3': 300.0}
+                   'mass1': 2.38827351798476, 'mass2': 2.94879810784617, 'width2': 5.30501566756126, 'width3': 0.0}
     true_paramsIsReal = {'etapi::imZ::resAmp1': True, 'etapi::imZ::resAmp2': False, 'etapi::imZ::resAmp3':
                          False, 'mass1': True, 'mass2': True, 'width2': True, 'width3': True}
     true_flat_params = [462.405000586143, 279.819030071884, -16.633970687601, 406.191515689351,
-                        164.391119358749, 1.0, 2.0, 200.0, 300.0]
+                        164.391119358749, 2.38827351798476, 2.94879810784617, 5.30501566756126, 0.0]
     true_names = ['Re[resAmp1]', 'Re[resAmp2]', 'Im[resAmp2]', 'Re[resAmp3]', 'Im[resAmp3]', 'mass1',
                   'mass2', 'width2', 'width3']
     truths = [true_params, true_paramsIsReal, true_flat_params, true_names]
