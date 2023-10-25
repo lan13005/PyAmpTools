6093f720d33f673d5c384dcd3db58811e437d07f
lan13005 <lng1492@gmail.com>
2023-10-24 22:10:08 -0400

    fix: add missing header files to build amptools and fsroot

---
+++ external/Distribution/initialize_fsroot.C
@@ -0,0 +1,6 @@
+#include "initialize_fsroot.h"
+
+void initialize_fsroot(bool verbose){
+	if (verbose)
+    	std::cout << "Initializing FSRoot for PyROOT..." << std::endl;
+}
---
+++ external/Distribution/initialize_fsroot.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void initialize_fsroot(bool verbose);
