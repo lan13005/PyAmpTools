65e8f89d3cf8bee1ac44d3d7ab0d1791224288e9
lan13005 <lng1492@gmail.com>
2025-02-05 10:59:32 -0500

    ift_pkl| add proxy command to iftPwaDx

---
+++ bin/ift_pkl_summary.py
@@ -0,0 +1,10 @@
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Summarize the contents of an iftpwa results pickle file")
+    parser.add_argument("pkl_file", type=str, help="Path to the pickle file")
+    args = parser.parse_args()
+    pkl_file = args.pkl_file
+    
+    os.system(f"iftPwaDx {pkl_file}")
