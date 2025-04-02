9e14b71e0035f3416fbfea7737a717d35f888e73
lan13005 <lng1492@gmail.com>
2025-04-01 20:05:44 -0400

    orchestrate| add moments plotting

--- OTHER_CHANNELS/ETAPI0_AUTOGRAD/orchestrate.py
+++ OTHER_CHANNELS/ETAPI0_AUTOGRAD/orchestrate.py
@@ -2,14 +2,14 @@ import os
 import random
 import tempfile
 from pyamptools.utility.general import load_yaml, recursive_replace, dump_yaml, execute_cmd
-from pyamptools.utility.resultManager import ResultManager, plot_binned_intensities, plot_binned_complex_plane, plot_overview_across_bins
+from pyamptools.utility.resultManager import ResultManager, plot_binned_intensities, plot_binned_complex_plane, plot_overview_across_bins, montage_and_gif_select_plots, plot_moments_across_bins
 from rich.console import Console
 
 console = Console()
 
-random.seed(12322)
-nseeds = 5
-seeds = random.sample(range(1000000), nseeds)
+random.seed(1420)
+nseeds = 20
+seeds = random.sample(range(10000000), nseeds)
 
 yaml_file = "/w/halld-scshelf2101/lng/WORK/PyAmpTools9/OTHER_CHANNELS/ETAPI0_AUTOGRAD/pyamptools_mc.yaml"
 
@@ -24,6 +24,8 @@ for seed in seeds:
     parent_directory = os.path.dirname(base_directory)
     recursive_replace(ift_yaml, base_directory, f"{parent_directory}/SEED_{seed}")
     recursive_replace(main_yaml, base_directory, f"{parent_directory}/SEED_{seed}")
+    
+    n_processes = main_yaml["n_mass_bins"]
 
     seed_directory = f"{parent_directory}/SEED_{seed}"
     os.makedirs(seed_directory, exist_ok=True)
@@ -40,7 +42,7 @@ for seed in seeds:
         execute_cmd([f"pa run_priorSim {tmp_main_yaml.name} -s {seed}"], console=console) # Generate data, divide data, ready to fit
         execute_cmd([f"pa run_mle {parent_directory}/SEED_{seed}/GENERATED/main.yaml"], console=console) # Run on new yaml files created by run_priorSim
         execute_cmd([f"pa run_ift {parent_directory}/SEED_{seed}/GENERATED/main.yaml"], console=console)
-        execute_cmd([f"pa run_mcmc {parent_directory}/SEED_{seed}/GENERATED/main.yaml -np 17 -nc 8 -nw 500 -ns 1000"], console=console)
+        execute_cmd([f"pa run_mcmc {parent_directory}/SEED_{seed}/GENERATED/main.yaml -np {n_processes} -nc 8 -nw 500 -ns 1000"], console=console)
 
 # NOTE: If we try to run the following commands in the above for-loop we run into a problem with the run_priorSim call
 #       on the second iteration over the seeds array. The mpiexec command silently fails. 
@@ -53,7 +55,10 @@ for seed in seeds:
 
     resultManager = ResultManager(f"{parent_directory}/SEED_{seed}/GENERATED/main.yaml")
     resultManager.attempt_load_all() # load all possible results (generated, data histogram, mle, mcmc, ift) in the 'base_directory' of the yaml file
+    resultManager.attempt_project_moments(pool_size=n_processes)
     plot_binned_intensities(resultManager)
     plot_binned_complex_plane(resultManager)
     plot_overview_across_bins(resultManager)
+    plot_moments_across_bins(resultManager)
+    montage_and_gif_select_plots(resultManager)
     del resultManager
